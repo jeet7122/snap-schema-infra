085f443fa61ca8399ecad032e70fa6639a2130d1
jeet7122 <jeetthakkar2612@gmail.com>
2026-05-20 18:23:46 -0500

    Add compile_sql function to generate SQL from SchemaResponse

--- utils/sql_compiler.py
+++ utils/sql_compiler.py
@@ -1,1 +1,10 @@
 from models.schema_models import SchemaResponse
+
+def compile_sql(schema: SchemaResponse):
+    sql_parts = []
+    
+    for table in schema.tables:
+        sql_parts.append(table.sql_query)
+        sql_parts.extend(table.indexes)
+        
+    return "\n\n".join(sql_parts)
