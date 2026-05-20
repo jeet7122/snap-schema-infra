3298f6e865a8611836f2218220d0cad2d2eff65c
jeet7122 <jeetthakkar2612@gmail.com>
2026-05-20 18:28:54 -0500

    Refactor build_metadata function for cleaner MetaData instantiation

--- utils/metadata_builder.py
+++ utils/metadata_builder.py
@@ -2,7 +2,4 @@ from models.schema_models import (SchemaResponse, MetaData)
 
 def build_metadata(schema: SchemaResponse):
     total_indexes = sum(len(table.indexes) for table in schema.tables)
-    return MetaData(
-        total_tables=len(schema.tables)
-        total_indexes=total_indexes
-    )
+    return MetaData(total_tables=len(schema.tables), total_indexes=total_indexes)
