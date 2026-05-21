a6ad1c2af0812e3e22478e78be476f61f0bf2bd4
jeet7122 <jeetthakkar2612@gmail.com>
2026-05-20 20:00:13 -0500

    Refactor run_validators function to improve readability and maintainability by organizing imports and enhancing code structure

--- utils/run_validators.py
+++ utils/run_validators.py
@@ -1,10 +1,46 @@
-from utils.validators.duplicate_index_validator import validate_duplicate_indexes
-from models.schema_models import SchemaResponse
+from utils.validators.duplicate_index_validator import (
+    validate_duplicate_indexes
+)
 
-def run_validators(schema: SchemaResponse):
+from utils.validators.foreign_key_validator import (
+    validate_foreign_key_indexes
+)
+
+from utils.validators.timestamp_validator import (
+    validate_timestamps
+)
+
+from utils.validators.enum_validator import (
+    validate_enum_modeling
+)
+
+from utils.validators.composite_index_validator import (
+    validate_composite_indexes
+)
+
+
+def run_validators(schema):
+
     warnings = []
-    
+
     warnings.extend(
         validate_duplicate_indexes(schema)
     )
+
+    warnings.extend(
+        validate_foreign_key_indexes(schema)
+    )
+
+    warnings.extend(
+        validate_timestamps(schema)
+    )
+
+    warnings.extend(
+        validate_enum_modeling(schema)
+    )
+
+    warnings.extend(
+        validate_composite_indexes(schema)
+    )
+
     return warnings
